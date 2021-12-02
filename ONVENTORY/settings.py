d5221b91ceea2130bcdecc3c9c64f740948733f8
Cedrouse Roll <omondicedo@gmail.com>
2021-12-02 04:11:54 +0300

    Update settings.py

--- ONVENTORY/settings.py
+++ ONVENTORY/settings.py
@@ -197,8 +197,8 @@ AWS_QUERYSTRING_AUTH = False
 
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
-# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
